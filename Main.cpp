c140a113ef323b7b563f76b8f0f5cf20d7d1aa4c
Thomas Martin <ThomasMartin98@hotmail.com>
2017-02-23 18:07:00 +0000

    Update Main.cpp
    Changed the node from a class to a struct

--- Main.cpp
+++ Main.cpp
@@ -13,44 +13,30 @@ const int width = 1920;
 const int height = 1080;
 
 // Class for the nodes which the user can place on the screen
-class Node {
-public:
-	
-	Node(sf::Vector2i mousePos);
-	sf::CircleShape getShape();
-	sf::Vector2f getPosition();
-private:
-	//Shape which represents the node
-	sf::CircleShape nodeShape;
-	//Its position in px
-	sf::Vector2i nodePos;
-};
 
-//Returns the position of the node
-sf::Vector2f Node::getPosition() {
-	return static_cast<sf::Vector2f>(this->nodePos);
+struct node {
+	sf::Vector2f nodePos;
+	void createNode(sf::Vector2i position);
+	sf::CircleShape nodeShape;
+};
+void node::createNode(sf::Vector2i position) {
+	nodePos = (sf::Vector2f)position;
+	nodeShape.setOrigin(10, 10);
+	nodeShape.setPosition(nodePos.x, nodePos.y);
+	nodeShape.setFillColor(sf::Color(255, 255, 255));
+	nodeShape.setRadius(10);
 }
-//Creates the node 
-Node::Node(sf::Vector2i mousePos) {
-	this->nodePos = mousePos;
-	this->nodeShape.setOrigin(10, 10);
-	this->nodeShape.setPosition(this->nodePos.x, this->nodePos.y);
-	this->nodeShape.setFillColor(sf::Color(255, 255, 255));
-	this->nodeShape.setRadius(10);
-}
-//Returns the shape of the node to be drawn
-sf::CircleShape Node::getShape() {
-	return this->nodeShape;
-}
-std::vector<Node> nodeArray;
+
+
+std::vector<node> nodeArray;
 std::vector<sf::VertexArray>edgeArray;
 std::vector<std::vector<double > > distanceMatrix;//n*n matrix
 
-//Creates an edge which will be pushed to the edgeArray vector
-void createEdge(sf::Vector2f node1Pos, sf::Vector2f node2Pos) {
+												  //Creates an edge which will be pushed to the edgeArray vector
+void createEdge(sf::Vector2f node1Pos, sf::Vector2f nodePos) {
 	sf::VertexArray newEdge(sf::Lines, 2);
 	newEdge[0].position = node1Pos;
-	newEdge[1].position = node2Pos;
+	newEdge[1].position = nodePos;
 	newEdge[0].color = sf::Color::Red;
 	newEdge[1].color = sf::Color::Red;
 	edgeArray.push_back(newEdge);
@@ -66,12 +52,12 @@ void createDistanceMatrix(sf::RenderWindow& window) {
 	for (int itr1 = 0; itr1<nodeArray.size(); itr1++) {
 		std::vector<double> newRow;
 		for (int itr2 = 0; itr2<nodeArray.size(); itr2++) {
-			node1 = nodeArray[itr1].getPosition();
-			node2 = nodeArray[itr2].getPosition();
+			node1 = (sf::Vector2f)nodeArray[itr1].nodePos;
+			node2 = (sf::Vector2f)nodeArray[itr2].nodePos;
 			dy = std::abs(node1.y - node2.y);
 			dx = std::abs(node1.x - node2.x);
 			distBetweenNodes = sqrt(pow(dx, 2) + pow(dy, 2));
-			//For whatever reason I cannot replace the lines below with edgeArray.push_back(node1,node2) even though it does the same thing!
+			//For whatever reason I cannot replace the lines below with edgeArray.push_back(node1,node) even though it does the same thing!
 			sf::VertexArray newEdge(sf::Lines, 2);
 			newEdge[0].position = node1;
 			newEdge[1].position = node2;
@@ -92,30 +78,32 @@ void createDistanceMatrix(sf::RenderWindow& window) {
 }
 //Prints the distance Matrix
 void printMatrix(std::vector < std::vector < double > > matrix) {
+	std::cout << "Begin matrix print" << '\n';
 	for (int row = 0; row < matrix.size(); row++) {
 		for (int col = 0; col < matrix.size(); col++) {
 			if (col == nodeArray.size() - 1) {
 				std::cout << matrix[row][col] << '\n';
 			}
 			else {
-				std::cout << matrix[row][col]<< '\t';
+				std::cout << matrix[row][col] << '\t';
 			}
 		}
 	}
+	std::cout << "End matrix print" << '\n';
 }
 
 // The algorithm for prim's with a distance matrix.
 void prims() {
 	edgeArray.clear();
-	sf::Vector2f node1, node2;
+	sf::Vector2f node1, node;
 	double smallestNum = sqrt(pow(height, 2) + pow(width, 2)); //The maximum distance between two nodes
-	int smallesNumPos=0;
+	int smallesNumPos = 0;
 	std::vector<int> allowedCol;// A list of all of the columns that can be searched
 	allowedCol.push_back(0);
-	for (int itr = 0; itr < nodeArray.size()  ;itr++) {
+	for (int itr = 0; itr < nodeArray.size(); itr++) {
 		double smallestNum = (width*height) + 1; //Should be sqrt(width^2+height^2)
 		int smallesNumPos = 0;
-		int previousNode =0;
+		int previousNode = 0;
 		printMatrix(distanceMatrix);
 		//Search matrix for smallest num
 		for (int col = 0; col < allowedCol.size(); col++) {
@@ -128,9 +116,9 @@ void prims() {
 			}
 		}
 		//Draw Line between nodes
-		node1 = nodeArray[previousNode].getPosition();
-		node2 = nodeArray[smallesNumPos].getPosition();
-		createEdge(node1,node2);
+		node1 = (sf::Vector2f)nodeArray[previousNode].nodePos;
+		node = (sf::Vector2f)nodeArray[smallesNumPos].nodePos;
+		createEdge(node1, node);
 		allowedCol.push_back(smallesNumPos);
 		for (int col2 = 0; col2 < distanceMatrix.size(); col2++) {
 			distanceMatrix[smallesNumPos][col2] = 0;
@@ -149,8 +137,9 @@ int main() {
 				window.close();
 				break;
 			case sf::Event::MouseButtonPressed: {
-				Node newNode(sf::Mouse::getPosition(window));
-				nodeArray.push_back(newNode);
+				node newNode1;
+				newNode1.createNode(sf::Mouse::getPosition(window));
+				nodeArray.push_back(newNode1);
 				break;
 			}
 			case sf::Event::KeyPressed:
@@ -161,7 +150,7 @@ int main() {
 		}
 		window.clear();
 		for (int itr = 0; itr<nodeArray.size(); itr++) {
-			window.draw(nodeArray[itr].getShape());
+			window.draw(nodeArray[itr].nodeShape);
 		}
 		for (int itr3 = 0; itr3<edgeArray.size(); itr3++) {
 			window.draw(edgeArray[itr3]);
@@ -170,4 +159,3 @@ int main() {
 	}
 	return 0;
 }
-
